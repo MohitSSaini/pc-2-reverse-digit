72ad0eb4ac6dd5a7a1b8d179c7f5a8e25bb98089
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-19 09:53:28 +0530

    All Test case Write and Pass all ReverseDigitTest.java

--- src/test/java/com/jap/ReverseDigitTest.java
+++ src/test/java/com/jap/ReverseDigitTest.java
@@ -1,24 +1,45 @@
 package com.jap;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ReverseDigitTest {
+    ReverseDigit reverseDigit;
 
-
-    @Before
+    @BeforeEach
     public void setUp() {
+        reverseDigit = new ReverseDigit ();
 
     }
-    @After
+    
+    @AfterEach
     public void tearDown() {
+        reverseDigit = null;
 
     }
-
-
-
-
+  
+    @Test
+    void givenInputPositiveNumberReverseTheDigit(){
+        int inputValue = 4758765;
+        //Expected Value
+        int expectedValue = 5678574;
+        //Inserting Value
+        int actualOutput = reverseDigit.getReverse(inputValue);
+        //Assertion
+        Assertions.assertEquals ( expectedValue,actualOutput );
+        
+    }
+    @Test
+    void givenInputNegativeNumberReverseTheDigit(){
+        int inputValue = -246567;
+        //Expected Value
+        int expectedValue = -765642;
+        //Inserting Value
+        int actualOutput = reverseDigit.getReverse(inputValue);
+        //Assertion
+        Assertions.assertEquals ( expectedValue,actualOutput );
+    }
 }
